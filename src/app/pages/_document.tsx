859f9a19915655ce20376fb4a7c4ac1ffae62f34
Alex Reinoso <ralexjessiel@gmail.com>
2024-06-17 08:46:21 -0500

    working on layouts

---
+++ src/app/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <html>
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </html>
+    )
+}
---
+++ src/app/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex flex-row justify-center">
+            <h1 className="text-center">Hello</h1>
+        </div>
+    )
+}
